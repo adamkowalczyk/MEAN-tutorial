201dfbb046cfe5920b67e399bc65ce047de26072
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-07-28 17:26:43 +0100

    posts

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-// define module
+// define module:
 var app = angular.module('flapperNews', []);
 
-// define controller
+// define controller:
 app.controller('MainCtrl', [
 '$scope',
+// $scope is the 'application object'.  
 function($scope){
   $scope.test = 'Hello world!';
+  $scope.posts = [
+    'post1',
+    'post2',
+    'post3',
+    'post4',
+    'post5'
+  ];
 }]);
