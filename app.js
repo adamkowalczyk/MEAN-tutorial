f80a29ab4d303b061086055e46a8abd6888b1b73
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-07-28 17:39:34 +0100

    upvote arrow

--- app.js
+++ app.js
@@ -7,19 +7,26 @@ app.controller('MainCtrl', [
 // $scope is the 'application object'.  
 function($scope){
   $scope.test = 'Hello world!';
+  
   $scope.posts = [
-   {title: 'post 1', upvotes: 5},
-   {title: 'post 2', upvotes: 2},
-   {title: 'post 3', upvotes: 15},
-   {title: 'post 4', upvotes: 9},
-   {title: 'post 5', upvotes: 4}
- ];
- $scope.addPost = function(){
-  // prevent empty posts:
-  if(!$scope.title || $scope.title === '') { return; }
-  // $scope.title comes from ng-model directive in html form
-  $scope.posts.push({title: $scope.title, upvotes: 0});
-  // reset variable..
-  $scope.title = '';
-};
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
+  ];
+ 
+  $scope.addPost = function(){
+    // prevent empty posts:
+    if(!$scope.title || $scope.title === '') { return; }
+    // $scope.title comes from ng-model directive in html form
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    // reset variable..
+    $scope.title = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+  
 }]);
