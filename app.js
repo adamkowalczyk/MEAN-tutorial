4f3a723c9b5fae037d88bd0282be86c3ef7d2f5e
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-07-29 09:27:10 +0100

    move data from controller to factory

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 // define module:
 var app = angular.module('flapperNews', []);
 
+//  define factory:
+// NB factories return an object (as opposed to services which create a constructor)
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
 // define controller:
+// NB Controllers are not for data -no persistance.
 app.controller('MainCtrl', [
 '$scope',
+'posts',
 // $scope is the 'application object'.  
-function($scope){
-  $scope.test = 'Hello world!';
+function($scope, posts){
   
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  // make factory object avaialble in $scope
+  $scope.posts = posts.posts;
+
+  // [
+  //   {title: 'post 1', upvotes: 5},
+  //   {title: 'post 2', upvotes: 2},
+  //   {title: 'post 3', upvotes: 15},
+  //   {title: 'post 4', upvotes: 9},
+  //   {title: 'post 5', upvotes: 4}
+  // ];
  
   $scope.addPost = function(){
     // prevent empty posts:
